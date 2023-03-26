d89875a09cacf15367bc10acdc1db52a2d1bc2a6
Aroyan Bakti Setiawan <abssneakers@gmail.com>
2023-03-26 21:40:46 +0800

    add config

---
+++ config/index.ts
@@ -0,0 +1,2 @@
+export const ALL_AIRPORTS_INDONESIA_URL =
+  "https://gist.githubusercontent.com/aroyan/89b170c9f76df8f76c31d3fe1b1d33df/raw/5b6f528c5cb2ccb3e519974d613a14651085d497/allAirportInIndonesia.json";
